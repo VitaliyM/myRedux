ff6a04761058448e3dd5967c17ba171645e31449
VitaliyM <vetalmamchur@gmail.com>
2023-02-02 15:29:30 +0000

    1

---
+++ rredux/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './main.css';
+import { connect } from 'react-redux';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default connect(
+  state => ({}),
+  dispatch => ({})
+);
